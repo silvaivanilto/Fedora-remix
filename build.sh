df5ff498d2094c1b232d8121809178491b947197
Ivanilton <silvaivanilto@outlook.com>
2024-12-07 21:51:33 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree override remove kdebugsettings firewall-config plasma-drkonqi
-rpm-ostree override install plasma-firewall-firewalld virt-install virt-manager virt-viewer 
+rpm-ostree remove kdebugsettings firewall-config plasma-drkonqi
+rpm-ostree install plasma-firewall-firewalld virt-install virt-manager virt-viewer 
 flatpak uninstall --all
 sudo rpm-ostree cleanup -m
 
